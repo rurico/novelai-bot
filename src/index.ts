c95094a0c90759f25915b3d4d5ec19909a73b33d
天海るり <babichan@outlook.com>
2022-10-20 12:26:54 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -365,7 +365,7 @@ export function apply(ctx: Context, config: Config) {
               n_samples: 'n_samples',
               seed: 'seed',
               sampler_index: 'sampler',
-              negative_prompt: 'uc',
+              negative_prompt: 'uc,fingers,fewer digits,extra limbs,extra arms,extra legs,malformed limbs,fused fingers,too many fingers,long neck,cross-eyed,mutated hands,polar lowres,bad anatomy,bad hands,bad body,bad proportions,gross proportions,text,error,missing fingers,missing arms,missing legs,extra digit,cropped,poorly drawn hands,poorly drawn face,mutation,deformed,worst quality,low quality,normal quality,jpeg artifacts,signature,watermark,lowres,bad anatomy,bad hands,text,error,missing fingers,extra digit,fewer digits,cropped,worstquality,low quality,normal quality,jpegartifacts,signature,watermark,username,blurry,bad feet',
             }),
           }
           : config.type === 'naifu'
